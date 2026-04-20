e70fadf94d6b0d9c532590f699f30a90196d9675
TudorDavid24 <tudor293david@gmail.com>
2026-04-20 10:00:49 +0200

    Aggiunta dell'esercizio cookies

---
+++ pagine/Cookies/script.js
@@ -0,0 +1,31 @@
+function setCookie(nome, valore, giorni){
+    let data = new Date();
+    data.setTime(data.getTime() + (giorni * 26 * 60 * 1000))
+    document.cookie = nome + 
+                        "=" + encodeURIComponent(valore) +
+                        ";expires=" + data.toUTCString() +
+                        ":path/";
+}
+
+function getCookie(nome){
+    let cookies = document.cookie.split(";");
+}
+
+function salvaDati(){
+    let nome = document.getElementById("nome").value;
+    let cognome = document.getElementById("cognome").value;
+    setCookie("nome", nome, 1);
+    setCookie("cognome", cognome, 1);
+    window.location.href = "pagina2.html";
+
+}
+
+function caricaRiepilogo(){
+    let nome = getCookie("nome");
+    let cognome = getCookie("cognome");
+    let s = "";
+    s += "Nome " + nome;
+    s += "<br>";
+    s += "Cognome " + cognome;
+    document.getElementById("riepilogo").innerHTML = s;
+}
