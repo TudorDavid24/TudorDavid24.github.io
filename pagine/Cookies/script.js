fbbd8706d74fc35621080d4eb4c7a45e9a94a688
TUDOR-PC\tudor <tudor293david@gmail.com>
2026-04-21 19:18:29 +0200

    Sistemato l'esercizio dei cookie e aggiunta l'integrazione con il css generale

--- pagine/Cookies/script.js
+++ pagine/Cookies/script.js
@@ -1,14 +1,20 @@
 function setCookie(nome, valore, giorni){
     let data = new Date();
-    data.setTime(data.getTime() + (giorni * 26 * 60 * 1000))
-    document.cookie = nome + 
-                        "=" + encodeURIComponent(valore) +
-                        ";expires=" + data.toUTCString() +
-                        ":path/";
+    data.setTime(data.getTime() + (giorni * 24 * 60 * 1000))
+    document.cookie = nome + "=" + encodeURIComponent(valore) + 
+                            ";expires=" + data.toUTCString() +
+                            ";path=/";
 }
 
 function getCookie(nome){
     let cookies = document.cookie.split(";");
+    for (let i=0; i < cookies.length; i++) {
+        let c = cookies[i].trim();
+        if (c.startsWith(nome + "=")) {
+            return decodeURIComponent(c.substring(nome.length + 1));
+        }
+    }
+    return "";
 }
 
 function salvaDati(){
@@ -24,8 +30,8 @@ function caricaRiepilogo(){
     let nome = getCookie("nome");
     let cognome = getCookie("cognome");
     let s = "";
-    s += "Nome " + nome;
+    s += "Nome: " + nome;
     s += "<br>";
-    s += "Cognome " + cognome;
+    s += "Cognome: " + cognome;
     document.getElementById("riepilogo").innerHTML = s;
 }
